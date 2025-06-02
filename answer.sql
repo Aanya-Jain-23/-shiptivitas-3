5c3f0d50877ad819dbeff7eb7c9b6b1cf070d7a7
Sai Shashank Vanga <svanga@unc.edu>
2025-06-02 09:51:11 -0500

    finished the sql code

--- answer.sql
+++ answer.sql
@@ -1,12 +1,35 @@
 -- TYPE YOUR SQL QUERY BELOW
 
 -- PART 1: Create a SQL query that maps out the daily average users before and after the feature change
+SELECT 
+  period,
+  COUNT(DISTINCT user_id) * 1.0 / COUNT(DISTINCT day) AS avg_daily_users
+FROM (
+  SELECT 
+    user_id,
+    DATE(login_timestamp, 'unixepoch') AS day,
+    CASE 
+      WHEN DATE(login_timestamp, 'unixepoch') < '2018-06-02' THEN 'Before'
+      ELSE 'After'
+    END AS period
+  FROM login_history
+)
+GROUP BY period;
+
 
 
 
 
 -- PART 2: Create a SQL query that indicates the number of status changes by card
 
+SELECT 
+  c.name,
+  COUNT(*) AS status_changes
+FROM card_change_history ch
+JOIN card c ON ch.cardId = c.id
+WHERE ch.oldStatus IS NOT ch.newStatus
+GROUP BY ch.cardId
+ORDER BY status_changes DESC;
 
 
 
